e78cacd990a643f7220d8e3489b6bcd832333735
Aniket <aniketchaudhari342@gmail.com>
2026-04-02 11:30:13 +0530

    All ERP and CRM files tables are created in bronze

--- bronze_layer/create_bronze_table_structure.sql
+++ bronze_layer/create_bronze_table_structure.sql
@@ -1,3 +1,7 @@
+--%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+--                   CRM Files
+--%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
 --=========================================================================
 -- Creating and replacing bronze.crm_cust_info
 --=========================================================================
@@ -35,6 +39,7 @@ CREATE TABLE bronze.crm_prd_info (
 --=========================================================================
 IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
     DROP TABLE bronze.crm_sales_details
+
 CREATE TABLE bronze.crm_sales_details(
     sls_ord_num NVARCHAR(50),
     sls_prd_key NVARCHAR(50),
@@ -45,4 +50,46 @@ CREATE TABLE bronze.crm_sales_details(
     sls_sales INT,
     sls_quantity INT,
     sls_price INT
+)
+
+
+
+--%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+--                   ERP Files
+--%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+--=========================================================================
+-- Creating and replacing bronze.erp_cust_az12
+--=========================================================================
+IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_cust_az12
+
+CREATE TABLE bronze.erp_cust_az12(
+    cid NVARCHAR(50),
+    bdate DATE,
+    gen NVARCHAR(50)
+)
+
+--=========================================================================
+-- Creating and replacing bronze.erp_loc_a101
+--=========================================================================
+IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_loc_a101
+
+CREATE TABLE bronze.erp_loc_a101 (
+    cid NVARCHAR(50),
+    cntry nvarchar(50)
+)
+
+--=========================================================================
+-- Creating and replacing bronze.erp_px_cat_g1v2
+--=========================================================================
+IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE bronze.erp_px_cat_g1v2
+
+CREATE TABLE bronze.erp_px_cat_g1v2(
+    id NVARCHAR(50),
+    cat NVARCHAR(50),
+    subcat NVARCHAR(50),
+    maintenance NVARCHAR(50)
 )
