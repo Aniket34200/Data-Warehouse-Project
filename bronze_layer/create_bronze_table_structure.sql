c6695277c1de6729ddb2f2e9f1015b70a9b1545c
Aniket <aniketchaudhari342@gmail.com>
2026-04-02 11:05:44 +0530

    All files from crm tables are created

--- bronze_layer/create_bronze_table_structure.sql
+++ bronze_layer/create_bronze_table_structure.sql
@@ -1,4 +1,8 @@
-CREATE SCHEMA bronze
+--=========================================================================
+-- Creating and replacing bronze.crm_cust_info
+--=========================================================================
+IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_cust_info
 
 CREATE TABLE bronze.crm_cust_info (
     cst_id INT,
@@ -8,4 +12,37 @@ CREATE TABLE bronze.crm_cust_info (
     cst_marital_status VARCHAR(50),
     cst_gndr VARCHAR(50),
     cst_create_date DATE
+)
+
+--=========================================================================
+-- Creating and replacing bronze.crm_prd_info
+--=========================================================================
+IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_prd_info
+
+CREATE TABLE bronze.crm_prd_info (
+    prd_id INT,
+    prd_key NVARCHAR(50),
+    prd_nm NVARCHAR(50),
+    prd_cost INT,
+    prd_line NVARCHAR(50),
+    prd_start_dt DATE,
+    prd_end_dt DATE
+)
+
+--=========================================================================
+-- Creating and replacing crm_sales_details
+--=========================================================================
+IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE bronze.crm_sales_details
+CREATE TABLE bronze.crm_sales_details(
+    sls_ord_num NVARCHAR(50),
+    sls_prd_key NVARCHAR(50),
+    sls_cust_id INT,
+    sls_order_dt INT,
+    sls_ship_dt INT,
+    sls_due_dt INT,
+    sls_sales INT,
+    sls_quantity INT,
+    sls_price INT
 )
