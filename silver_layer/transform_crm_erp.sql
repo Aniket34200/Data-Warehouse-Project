07806253a8ea29c31cbefdd585d4230441e47628
Aniket <aniketchaudhari342@gmail.com>
2026-04-03 17:59:04 +0530

    stored procedure added and error handling while insertion of data

--- silver_layer/transform_crm_erp.sql
+++ silver_layer/transform_crm_erp.sql
@@ -1,198 +1,210 @@
 --*************************************************************************
 --          Data insertion in Silver using Stored Procedure
 --*************************************************************************
+CREATE OR ALTER PROCEDURE silver.load_silver
+AS 
+BEGIN
 
---=========================================================================
---                       silver.crm_cust_info
---=========================================================================
-TRUNCATE TABLE silver.crm_cust_info;
+    BEGIN TRY
+        --=========================================================================
+        --                       silver.crm_cust_info
+        --=========================================================================
+        TRUNCATE TABLE silver.crm_cust_info;
 
-INSERT INTO silver.crm_cust_info (
-    cst_id, 
-    cst_key, 
-    cst_firstname, 
-    cst_lastname, 
-    cst_marital_status,
-    cst_gndr, 
-    cst_create_date
-)
+        INSERT INTO silver.crm_cust_info (
+            cst_id, 
+            cst_key, 
+            cst_firstname, 
+            cst_lastname, 
+            cst_marital_status,
+            cst_gndr, 
+            cst_create_date
+        )
 
-SELECT 
-    cst_id, 
-    cst_key, 
-    TRIM(cst_firstname) as cst_firstname,   -- remove extra spaces from first name
-    TRIM(cst_lastname) as cst_lastname,    -- remove extra spaces from last name
-    CASE 
-        WHEN UPPER(TRIM(cst_marital_status)) = 'M' then 'Married'
-        WHEN UPPER(TRIM(cst_marital_status)) = 'S' then 'Single'
-        ELSE 'Unknown'
-    END as cst_marital_status,   -- normalize maritial status into readable format 
-    CASE 
-        WHEN UPPER(TRIM(cst_gndr)) = 'M' then 'Male'
-        WHEN UPPER(TRIM(cst_gndr)) = 'F' then 'Female'
-        ELSE 'Unknown'
-    END as cst_gndr,     -- normalize gender into readable format
-    cst_create_date
-FROM (
-    SELECT 
-        *,
-        ROW_NUMBER() OVER(PARTITION BY cst_id ORDER BY cst_create_date desc) as flag
-    FROM bronze.crm_cust_info
-)t 
-where flag = 1 AND cst_id IS NOT NULL;   -- select the most recent record and non null values
-
-
-
---=========================================================================
---                      silver.crm_prd_info
---=========================================================================
-TRUNCATE TABLE silver.crm_prd_info;
-
-INSERT INTO silver.crm_prd_info(
-    prd_id,
-    cat_id,
-    prd_key,
-    prd_nm,
-    prd_cost,
-    prd_line,
-    prd_start_dt,
-    prd_end_dt
-)
-
-SELECT
-    prd_id,
-    REPLACE(SUBSTRING(prd_key, 1, 5),'-','_') AS cat_id,
-    SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,
-    prd_nm, 
-    ISNULL(prd_cost,0) AS prd_cost, 
-    CASE 
-        WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
-        WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
-        WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
-        WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
-        ELSE 'Unknown'
-    END AS prd_line, 
-    prd_start_dt,
-    DATEADD(DAY, -1, 
-        LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt)
-    ) AS prd_prev_end_dt
-FROM bronze.crm_prd_info;
+        SELECT 
+            cst_id, 
+            cst_key, 
+            TRIM(cst_firstname) as cst_firstname,   -- remove extra spaces from first name
+            TRIM(cst_lastname) as cst_lastname,    -- remove extra spaces from last name
+            CASE 
+                WHEN UPPER(TRIM(cst_marital_status)) = 'M' then 'Married'
+                WHEN UPPER(TRIM(cst_marital_status)) = 'S' then 'Single'
+                ELSE 'Unknown'
+            END as cst_marital_status,   -- normalize maritial status into readable format 
+            CASE 
+                WHEN UPPER(TRIM(cst_gndr)) = 'M' then 'Male'
+                WHEN UPPER(TRIM(cst_gndr)) = 'F' then 'Female'
+                ELSE 'Unknown'
+            END as cst_gndr,     -- normalize gender into readable format
+            cst_create_date
+        FROM (
+            SELECT 
+                *,
+                ROW_NUMBER() OVER(PARTITION BY cst_id ORDER BY cst_create_date desc) as flag
+            FROM bronze.crm_cust_info
+        )t 
+        where flag = 1 AND cst_id IS NOT NULL;   -- select the most recent record and non null values
 
 
 
---=========================================================================
---                      silver.crm_sales_details
---=========================================================================
-TRUNCATE TABLE silver.crm_sales_details;
+        --=========================================================================
+        --                      silver.crm_prd_info
+        --=========================================================================
+        TRUNCATE TABLE silver.crm_prd_info;
 
-INSERT INTO silver.crm_sales_details (
-    sls_ord_num,
-    sls_prd_key,
-    sls_cust_id, 
-    sls_order_dt,
-    sls_ship_dt, 
-    sls_due_dt,
-    sls_sales,
-    sls_quantity,
-    sls_price
-)
+        INSERT INTO silver.crm_prd_info(
+            prd_id,
+            cat_id,
+            prd_key,
+            prd_nm,
+            prd_cost,
+            prd_line,
+            prd_start_dt,
+            prd_end_dt
+        )
 
-SELECT
-    sls_ord_num,
-    sls_prd_key,
-    sls_cust_id, 
-    CASE 
-        WHEN sls_order_dt = 0 OR LEN(sls_order_dt) <> 8 THEN NULL
-        ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
-    END AS sls_order_dt,
-    CASE 
-        WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) <> 8 THEN NULL
-        ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
-    END AS sls_ship_dt,
-    CASE 
-        WHEN sls_due_dt = 0 OR LEN(sls_due_dt) <> 8 THEN NULL
-        ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
-    END AS sls_due_dt,
-    CASE 
-        WHEN sls_sales IS NULL OR sls_sales<=0 OR sls_sales <> sls_quantity * ABS(sls_price)
-            THEN sls_quantity * ABS(sls_price)
-        ELSE sls_sales
-    END as sls_sales,
-    sls_quantity,
-    CASE 
-        WHEN sls_price IS NULL OR sls_price<=0 
-            THEN sls_sales / NULLIF(sls_quantity,0)
-        Else sls_price 
-    END AS sls_price 
-FROM bronze.crm_sales_details;
+        SELECT
+            prd_id,
+            REPLACE(SUBSTRING(prd_key, 1, 5),'-','_') AS cat_id,
+            SUBSTRING(prd_key, 7, LEN(prd_key)) AS prd_key,
+            prd_nm, 
+            ISNULL(prd_cost,0) AS prd_cost, 
+            CASE 
+                WHEN UPPER(TRIM(prd_line)) = 'R' THEN 'Road'
+                WHEN UPPER(TRIM(prd_line)) = 'M' THEN 'Mountain'
+                WHEN UPPER(TRIM(prd_line)) = 'S' THEN 'Other Sales'
+                WHEN UPPER(TRIM(prd_line)) = 'T' THEN 'Touring'
+                ELSE 'Unknown'
+            END AS prd_line, 
+            prd_start_dt,
+            DATEADD(DAY, -1, 
+                LEAD(prd_start_dt) OVER(PARTITION BY prd_key ORDER BY prd_start_dt)
+            ) AS prd_prev_end_dt
+        FROM bronze.crm_prd_info;
 
 
 
---=========================================================================
---                      silver.erp_cust_az12
---=========================================================================
-TRUNCATE TABLE silver.erp_cust_az12;
+        --=========================================================================
+        --                      silver.crm_sales_details
+        --=========================================================================
+        TRUNCATE TABLE silver.crm_sales_details;
 
-INSERT INTO silver.erp_cust_az12 (
-    cid,
-    bdate,
-    gen
-)
+        INSERT INTO silver.crm_sales_details (
+            sls_ord_num,
+            sls_prd_key,
+            sls_cust_id, 
+            sls_order_dt,
+            sls_ship_dt, 
+            sls_due_dt,
+            sls_sales,
+            sls_quantity,
+            sls_price
+        )
 
-SELECT
-    CASE 
-        WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid))
-        ELSE cid 
-    END AS cid, 
-    bdate,
-    CASE 
-        WHEN UPPER(TRIM(gen)) IN ('F','FEMALE') THEN 'Female' 
-        WHEN UPPER(TRIM(gen)) IN ('M','MALE') THEN 'Male'
-        ELSE 'Unknown'
-    END AS gen
-FROM bronze.erp_cust_az12;
-
-
-
---=========================================================================
---                      silver.erp_loc_a101
---=========================================================================
-TRUNCATE TABLE silver.erp_loc_a101;
-
-INSERT INTO silver.erp_loc_a101 (
-    cid, 
-    country
-)
-
-SELECT
-    REPLACE(cid, '-','') AS cid, 
-    CASE 
-        WHEN country = 'DE' THEN 'Germany' 
-        WHEN country IN ('USA', 'US') THEN 'United States'
-        WHEN country = '' OR country IS NULL THEN 'Unknown'
-        ELSE country 
-    END AS country
-FROM bronze.erp_loc_a101;
-
-
-
---=========================================================================
---                      silver.erp_px_cat_g1v2
---=========================================================================
-TRUNCATE TABLE silver.erp_px_cat_g1v2;
-
-INSERT INTO silver.erp_px_cat_g1v2 (
-    id, 
-    cat,
-    subcat,
-    maintenance
-)
-
-SELECT
-    id,
-    cat,
-    subcat,
-    maintenance
-FROM bronze.erp_px_cat_g1v2;
+        SELECT
+            sls_ord_num,
+            sls_prd_key,
+            sls_cust_id, 
+            CASE 
+                WHEN sls_order_dt = 0 OR LEN(sls_order_dt) <> 8 THEN NULL
+                ELSE CAST(CAST(sls_order_dt AS VARCHAR) AS DATE)
+            END AS sls_order_dt,
+            CASE 
+                WHEN sls_ship_dt = 0 OR LEN(sls_ship_dt) <> 8 THEN NULL
+                ELSE CAST(CAST(sls_ship_dt AS VARCHAR) AS DATE)
+            END AS sls_ship_dt,
+            CASE 
+                WHEN sls_due_dt = 0 OR LEN(sls_due_dt) <> 8 THEN NULL
+                ELSE CAST(CAST(sls_due_dt AS VARCHAR) AS DATE)
+            END AS sls_due_dt,
+            CASE 
+                WHEN sls_sales IS NULL OR sls_sales<=0 OR sls_sales <> sls_quantity * ABS(sls_price)
+                    THEN sls_quantity * ABS(sls_price)
+                ELSE sls_sales
+            END as sls_sales,
+            sls_quantity,
+            CASE 
+                WHEN sls_price IS NULL OR sls_price<=0 
+                    THEN sls_sales / NULLIF(sls_quantity,0)
+                Else sls_price 
+            END AS sls_price 
+        FROM bronze.crm_sales_details;
 
 
+
+        --=========================================================================
+        --                      silver.erp_cust_az12
+        --=========================================================================
+        TRUNCATE TABLE silver.erp_cust_az12;
+
+        INSERT INTO silver.erp_cust_az12 (
+            cid,
+            bdate,
+            gen
+        )
+
+        SELECT
+            CASE 
+                WHEN cid LIKE 'NAS%' THEN SUBSTRING(cid, 4, LEN(cid))
+                ELSE cid 
+            END AS cid, 
+            bdate,
+            CASE 
+                WHEN UPPER(TRIM(gen)) IN ('F','FEMALE') THEN 'Female' 
+                WHEN UPPER(TRIM(gen)) IN ('M','MALE') THEN 'Male'
+                ELSE 'Unknown'
+            END AS gen
+        FROM bronze.erp_cust_az12;
+
+
+
+        --=========================================================================
+        --                      silver.erp_loc_a101
+        --=========================================================================
+        TRUNCATE TABLE silver.erp_loc_a101;
+
+        INSERT INTO silver.erp_loc_a101 (
+            cid, 
+            country
+        )
+
+        SELECT
+            REPLACE(cid, '-','') AS cid, 
+            CASE 
+                WHEN country = 'DE' THEN 'Germany' 
+                WHEN country IN ('USA', 'US') THEN 'United States'
+                WHEN country = '' OR country IS NULL THEN 'Unknown'
+                ELSE country 
+            END AS country
+        FROM bronze.erp_loc_a101;
+
+
+
+        --=========================================================================
+        --                      silver.erp_px_cat_g1v2
+        --=========================================================================
+        TRUNCATE TABLE silver.erp_px_cat_g1v2;
+
+        INSERT INTO silver.erp_px_cat_g1v2 (
+            id, 
+            cat,
+            subcat,
+            maintenance
+        )
+
+        SELECT
+            id,
+            cat,
+            subcat,
+            maintenance
+        FROM bronze.erp_px_cat_g1v2;
+    
+    END TRY
+    BEGIN CATCH 
+        PRINT '---------------Error Occurred--------------------';
+        PRINT 'Error Message:' + ERROR_MESSAGE();
+        PRINT 'Error Number:' + ERROR_NUMBER();
+        PRINT 'Error Line:' + ERROR_LINE();
+    END CATCH
+
+END
