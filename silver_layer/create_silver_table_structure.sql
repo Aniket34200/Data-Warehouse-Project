adc2e5225ae2fc46fb407cd66aa9185507328d7d
Aniket <aniketchaudhari342@gmail.com>
2026-04-03 10:09:20 +0530

    solve silver layer schema problem

--- silver_layer/create_silver_table_structure.sql
+++ silver_layer/create_silver_table_structure.sql
@@ -1,13 +1,14 @@
 --*************************************************************************
 --                            CRM Files
 --*************************************************************************
---=========================================================================
--- Creating and replacing bronze.crm_cust_info
---=========================================================================
-IF OBJECT_ID('bronze.crm_cust_info', 'U') IS NOT NULL
-    DROP TABLE bronze.crm_cust_info
 
-CREATE TABLE bronze.crm_cust_info (
+--=========================================================================
+-- Creating and replacing silver.crm_cust_info
+--=========================================================================
+IF OBJECT_ID('silver.crm_cust_info', 'U') IS NOT NULL
+    DROP TABLE silver.crm_cust_info
+
+CREATE TABLE silver.crm_cust_info (
     cst_id INT,
     cst_key NVARCHAR(50),
     cst_firstname VARCHAR(50),
@@ -18,12 +19,12 @@ CREATE TABLE bronze.crm_cust_info (
 )
 
 --=========================================================================
--- Creating and replacing bronze.crm_prd_info
+-- Creating and replacing silver.crm_prd_info
 --=========================================================================
-IF OBJECT_ID('bronze.crm_prd_info', 'U') IS NOT NULL
-    DROP TABLE bronze.crm_prd_info
+IF OBJECT_ID('silver.crm_prd_info', 'U') IS NOT NULL
+    DROP TABLE silver.crm_prd_info
 
-CREATE TABLE bronze.crm_prd_info (
+CREATE TABLE silver.crm_prd_info (
     prd_id INT,
     prd_key NVARCHAR(50),
     prd_nm NVARCHAR(50),
@@ -36,10 +37,10 @@ CREATE TABLE bronze.crm_prd_info (
 --=========================================================================
 -- Creating and replacing crm_sales_details
 --=========================================================================
-IF OBJECT_ID('bronze.crm_sales_details', 'U') IS NOT NULL
-    DROP TABLE bronze.crm_sales_details
+IF OBJECT_ID('silver.crm_sales_details', 'U') IS NOT NULL
+    DROP TABLE silver.crm_sales_details
 
-CREATE TABLE bronze.crm_sales_details(
+CREATE TABLE silver.crm_sales_details(
     sls_ord_num NVARCHAR(50),
     sls_prd_key NVARCHAR(50),
     sls_cust_id INT,
@@ -58,35 +59,35 @@ CREATE TABLE bronze.crm_sales_details(
 --*************************************************************************
 
 --=========================================================================
--- Creating and replacing bronze.erp_cust_az12
+-- Creating and replacing silver.erp_cust_az12
 --=========================================================================
-IF OBJECT_ID('bronze.erp_cust_az12', 'U') IS NOT NULL
-    DROP TABLE bronze.erp_cust_az12
+IF OBJECT_ID('silver.erp_cust_az12', 'U') IS NOT NULL
+    DROP TABLE silver.erp_cust_az12
 
-CREATE TABLE bronze.erp_cust_az12(
+CREATE TABLE silver.erp_cust_az12(
     cid NVARCHAR(50),
     bdate DATE,
     gen NVARCHAR(50)
 )
 
 --=========================================================================
--- Creating and replacing bronze.erp_loc_a101
+-- Creating and replacing silver.erp_loc_a101
 --=========================================================================
-IF OBJECT_ID('bronze.erp_loc_a101', 'U') IS NOT NULL
-    DROP TABLE bronze.erp_loc_a101
+IF OBJECT_ID('silver.erp_loc_a101', 'U') IS NOT NULL
+    DROP TABLE silver.erp_loc_a101
 
-CREATE TABLE bronze.erp_loc_a101 (
+CREATE TABLE silver.erp_loc_a101 (
     cid NVARCHAR(50),
     country nvarchar(50)
 )
 
 --=========================================================================
--- Creating and replacing bronze.erp_px_cat_g1v2
+-- Creating and replacing silver.erp_px_cat_g1v2
 --=========================================================================
-IF OBJECT_ID('bronze.erp_px_cat_g1v2', 'U') IS NOT NULL
-    DROP TABLE bronze.erp_px_cat_g1v2
+IF OBJECT_ID('silver.erp_px_cat_g1v2', 'U') IS NOT NULL
+    DROP TABLE silver.erp_px_cat_g1v2
 
-CREATE TABLE bronze.erp_px_cat_g1v2(
+CREATE TABLE silver.erp_px_cat_g1v2(
     id NVARCHAR(50),
     cat NVARCHAR(50),
     subcat NVARCHAR(50),
